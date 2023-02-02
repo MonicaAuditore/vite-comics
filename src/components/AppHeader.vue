1842c4b9ad3baeceb84df987720f0646dafd13c8
MonicaAuditore <mauditore@live.it>
2023-02-02 14:26:13 +0100

    add_logo

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+import { createAssignmentExpression } from "@vue/compiler-core";
+
+export default {
+  data() {
+    return {
+      logo: "dc-logo.png",
+    };
+  },
+  methods: {
+    getImagePath: function (imgPath) {
+      return new URL(imgPath, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="logo">
+        <img :src="getImagePath(`../assets/img/${logo}`)" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 1000px;
+  margin: 0 auto;
+}
+</style>
