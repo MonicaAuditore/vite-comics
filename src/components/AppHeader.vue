21122f2df5859cf6fc52af211aa7cd6c70d50456
MonicaAuditore <mauditore@live.it>
2023-02-02 14:39:28 +0100

    add_header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -17,17 +17,59 @@ export default {
 
 <template>
   <div class="header">
-    <div class="container">
-      <div class="logo">
-        <img :src="getImagePath(`../assets/img/${logo}`)" />
+    <div class="mainContainer">
+      <div class="container">
+        <div class="logo">
+          <img :src="getImagePath(`../assets/img/${logo}`)" />
+        </div>
+        <div class="menu">
+          <ul>
+            <li>CHARACTERS</li>
+            <li>COMICS</li>
+            <li>MOVIES</li>
+            <li>TV</li>
+            <li>GAMES</li>
+            <li>COLLECTIBLES</li>
+            <li>VIDEOS</li>
+            <li>FANS</li>
+            <li>NEWS</li>
+            <li>SHOP</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.container {
-  width: 1000px;
+.mainContainer {
+  width: 1444px;
   margin: 0 auto;
+}
+
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: antiquewhite;
+  height: 110px;
+}
+
+.menu {
+  line-height: 110px;
+}
+
+.menu ul {
+  display: flex;
+}
+
+.menu ul li {
+  list-style: none;
+  padding: 0px 10px;
+}
+
+.menu ul li:hover {
+  box-shadow: inset 0em -0.3em #0282f9;
 }
 </style>
